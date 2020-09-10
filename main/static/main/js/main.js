31ef727ea198941704e51b68846c8bcb8d797bdd
Sergey <kurakoff19@gmail.com>
2020-09-10 13:33:45 +0300

    Add answer

--- main/static/main/js/main.js
+++ main/static/main/js/main.js
@@ -47,3 +47,21 @@ $(document).ready(function() {
 
 });
 
+
+$(function() {
+  // copy content to clipboard
+  function copyToClipboard(element) {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val($(element).text()).select();
+    document.execCommand("copy");
+    $temp.remove();
+  }
+
+  // copy coupone code to clipboard
+  $(".coupon-btn").on("click", function() {
+    copyToClipboard("#coupon-field");
+    $(".coupon-alert").fadeIn("slow");
+  });
+});
+
